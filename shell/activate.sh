d9da7f4de9aae6f5c1fad5527bb7f31e65ffba6b
Ingo Karkat <swdev@ingo-karkat.de>
2020-01-28 15:27:13 +0100

    tmux-activate-on-fail: Also integrate with alert script

--- shell/activate.sh
+++ shell/activate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh source-this-script
 
-alias tmux-activate-on-fail='pipelinemunge _PS1OnFail tmux-activate-pane'
+alias tmux-activate-on-fail='pipelinemunge _PS1OnFail tmux-activate-pane; pipelinemunge RUNWITHPROMPT_FAIL_COMMAND tmux-activate-pane; export RUNWITHPROMPT_FAIL_COMMAND'
 alias tmux-activate-on-fail-alert='export ALERT_FAIL_COMMAND=tmux-activate-pane'
