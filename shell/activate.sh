de3701efcfee4347227afc10a7f90c4de5e16f88
Ingo Karkat <swdev@ingo-karkat.de>
2020-01-31 21:50:57 +0100

    Use the resolved tmux commands instead of tmux-activate-pane in the tmujx-activate-on-fail aliases
    The pane activation also doesn't work from inside my rvtt; probably, the same problem with pipelining.
    So just resolve the actual tmux commands for activation and directly embed that inside the alias. As long as that shell only is visible in a single session and pane, that should not make any difference.

--- shell/activate.sh
+++ shell/activate.sh
@@ -1,4 +1,6 @@
 #!/bin/sh source-this-script
 
-alias tmux-activate-on-fail='pipelinemunge _PS1OnFail tmux-activate-pane; pipelinemunge RUNWITHPROMPT_FAIL_COMMAND tmux-activate-pane; export RUNWITHPROMPT_FAIL_COMMAND'
-alias tmux-activate-on-fail-alert='export ALERT_FAIL_COMMAND=tmux-activate-pane'
+_tmuxActivatePaneCommand="$(tmux-activate-pane --get-command)"
+alias tmux-activate-on-fail="pipelinemunge _PS1OnFail '${_tmuxActivatePaneCommand}'; pipelinemunge RUNWITHPROMPT_FAIL_COMMAND '${_tmuxActivatePaneCommand}'; export RUNWITHPROMPT_FAIL_COMMAND"
+alias tmux-activate-on-fail-alert="export ALERT_FAIL_COMMAND='${_tmuxActivatePaneCommand}'"
+unset _tmuxActivatePaneCommand
