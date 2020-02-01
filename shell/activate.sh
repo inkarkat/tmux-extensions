82a591a1d0abfadf22facd12a6c36064e9eacb23
Ingo Karkat <swdev@ingo-karkat.de>
2020-02-01 22:21:08 +0100

    Only define aliases if inside tmux
    As we're actively querying the tmux pane / window during sourcing now, this is needed to avoid the tmux warning. Also, it doesn't make sense to define these when tmux isn't active. (And starting tmux will source the script again in the next shell.)

--- shell/activate.sh
+++ shell/activate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh source-this-script
 
+[ "${TMUX:-}" ] || return
+
 _tmuxActivatePaneCommand="$(tmux-activate-pane --get-command)"
 alias tmux-activate-on-fail="pipelinemunge _PS1OnFail '${_tmuxActivatePaneCommand}'; pipelinemunge RUNWITHPROMPT_FAIL_COMMAND '${_tmuxActivatePaneCommand}'; export RUNWITHPROMPT_FAIL_COMMAND"
 alias tmux-activate-on-fail-alert="export ALERT_FAIL_COMMAND='${_tmuxActivatePaneCommand}'"
