f3a214548bc2c36bf6dd37dbc192c7b97a172554
Ingo Karkat <swdev@ingo-karkat.de>
2020-11-16 08:16:36 +0100

    Allow definition of tmux aliases as "tmux foo"
    This previously was part of my personal configuration, but belongs here.

---
+++ shell/wrappers.sh
@@ -0,0 +1,16 @@
+#!/bin/sh source-this-script
+
+# Allow definition of tmux aliases (e.g. "tmux foo") by putting an executable
+# "tmux-foo" somewhere in the PATH.
+tmux()
+{
+    typeset tmuxAlias="tmux-$1"
+    if [ $# -eq 0 ]; then
+	tmux-wrapper ${TMUX_DEFAULT_COMMAND:-new-session}
+    elif type ${BASH_VERSION:+-t} "$tmuxAlias" >/dev/null 2>&1; then
+	shift
+	eval $tmuxAlias '"$@"'
+    else
+	tmux-wrapper "$@"
+    fi
+}
