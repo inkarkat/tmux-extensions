e7122bc06377c8849d89d013171ef1b19fb2be5a
Ingo Karkat <swdev@ingo-karkat.de>
2022-06-12 10:11:17 +0200

    ENH: Execute a passed COMMAND in a new tmux window

--- shell/wrappers.sh
+++ shell/wrappers.sh
@@ -2,6 +2,8 @@
 
 # Allow definition of tmux aliases (e.g. "tmux foo") by putting an executable
 # "tmux-foo" somewhere in the PATH.
+# Execute a passed COMMAND in a new tmux window (that automatically closes after
+# COMMAND concludes.)
 tmux()
 {
     typeset tmuxAlias="tmux-$1"
@@ -10,6 +12,8 @@ tmux()
     elif type ${BASH_VERSION:+-t} "$tmuxAlias" >/dev/null 2>&1; then
 	shift
 	$tmuxAlias "$@"
+    elif type ${BASH_VERSION:+-t} "$1" >/dev/null; then
+	tmux-wrapper set status on \; new-window -c "#{pane_current_path}" "$@"
     else
 	tmux-wrapper "$@"
     fi
