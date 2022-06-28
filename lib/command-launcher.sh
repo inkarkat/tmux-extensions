6b539a1fc3166ae41794f2cff33d273a4d4903a2
Ingo Karkat <swdev@ingo-karkat.de>
2022-06-28 20:05:31 +0200

    command-launcher: FIX: Need to exit, not return

--- lib/command-launcher.sh
+++ lib/command-launcher.sh
@@ -15,5 +15,5 @@ if [ $SECONDS -le ${TMUXCOMMANDLAUNCHER_PROMPT_TIME:-2} ]; then
     userprompt
 fi
 
-tmuxCurrentWindowNum="$(tmux display-message -p '#{session_windows}' 2>/dev/null)" || return
+tmuxCurrentWindowNum="$(tmux display-message -p '#{session_windows}' 2>/dev/null)" || exit
 [ $tmuxCurrentWindowNum -gt 2 ] || tmux set-option status off 2>/dev/null   # As we're still in the about-to-be-closed window, the threshold for clearing the status needs to be +1.
