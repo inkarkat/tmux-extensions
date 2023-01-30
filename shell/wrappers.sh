9e73ab83cf4be58f8ed843b2caee18206ddc8507
Ingo Karkat <swdev@ingo-karkat.de>
2023-01-30 08:15:09 +0100

    wrappers: BUG: Minor: Error on "tmux -V"
    > bash: type: -V: invalid option

--- shell/wrappers.sh
+++ shell/wrappers.sh
@@ -12,7 +12,7 @@ tmux()
     elif type ${BASH_VERSION:+-t} "$tmuxAlias" >/dev/null 2>&1; then
 	shift
 	$tmuxAlias "$@"
-    elif type ${BASH_VERSION:+-t} "$1" >/dev/null; then
+    elif type ${BASH_VERSION:+-t} -- "$1" >/dev/null; then
 	typeset scriptDir="$([ "${BASH_SOURCE[0]}" ] && dirname -- "${BASH_SOURCE[0]}" || exit 3)"
 	[ -d "$scriptDir" ] || { echo >&2 'ERROR: Cannot determine script directory!'; exit 3; }
 	typeset projectDir="${scriptDir}/.."
