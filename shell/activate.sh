44ce4dfdb980199e83a2802714c80ff896ae9366
Ingo Karkat <swdev@ingo-karkat.de>
2023-04-02 22:09:57 +0200

    activate: Adapt: pipelinemunge has been renamed to commandSequenceMunge

--- shell/activate.sh
+++ shell/activate.sh
@@ -3,6 +3,6 @@
 [ "${TMUX:-}" ] || return
 
 _tmuxActivatePaneCommand="$(tmux-activate-pane --get-command)"
-alias tmux-activate-on-fail="pipelinemunge _PS1OnFail '${_tmuxActivatePaneCommand}'; pipelinemunge RUNWITHPROMPT_FAIL_COMMAND '${_tmuxActivatePaneCommand}'; export RUNWITHPROMPT_FAIL_COMMAND"
+alias tmux-activate-on-fail="commandSequenceMunge _PS1OnFail '${_tmuxActivatePaneCommand}'; commandSequenceMunge RUNWITHPROMPT_FAIL_COMMAND '${_tmuxActivatePaneCommand}'; export RUNWITHPROMPT_FAIL_COMMAND"
 alias tmux-activate-on-fail-alert="export ALERT_FAIL_COMMAND='${_tmuxActivatePaneCommand}'"
 unset _tmuxActivatePaneCommand
