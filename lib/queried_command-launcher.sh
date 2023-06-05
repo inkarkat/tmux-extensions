d8ceda0cd8d44486e229b435fe8cbc9edd459fe1
Ingo Karkat <swdev@ingo-karkat.de>
2023-06-05 16:28:35 +0200

    queried_command-launcher: Use an overridden default-command to run the queried command e.g. on the remote system instead of locally
    So that prefix + - works like prefix + c / prefix + % / prefix + _ when issued in a tssh window.
    Note that I won't support the prefix + g c/%/_ variant that keeps the command local, as these mappings are already taken for re-running the same command again without querying.

--- lib/queried_command-launcher.sh
+++ lib/queried_command-launcher.sh
@@ -3,8 +3,13 @@
 queriedCommand="$(tmux show-options -gv @queried_command)"
 [ -n "$queriedCommand" ] || exit 99
 
+# Use an overridden default-command to run the queried command e.g. on the
+# remote system instead of locally.
+defaultCommand="$(tmux show-options -Aqv default-command)"
+[ -z "$defaultCommand" ] || [ "$defaultCommand" != "$(tmux show-options -Aqv default-shell)" ] || defaultCommand=''
+
 PGID=$$ \
 TERM_COLORS=256 \
     exec \
 	ensurePrompting --prompt 'Press any key to dismiss...' -- \
-	    runWithPrompt --command "$queriedCommand"
+	    runWithPrompt --command "${defaultCommand}${defaultCommand:+ }$queriedCommand"
