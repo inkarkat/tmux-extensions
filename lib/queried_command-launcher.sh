17609b2cc80749f4e71bef6402f75b80cda4fc95
Ingo Karkat <swdev@ingo-karkat.de>
2023-07-05 11:46:37 +0200

    queried_command-launcher: ENH: Allow setting the window name via optional flag
    This is used by my new prefix + * mapping that is a variant of prefix + - / prefix + |, using a new window instead of a split pane.
    Like prefix + c, we want to use the command's name as the window name, and indicate a concluded command by putting its name in round parentheses, just like new-window-launcher.sh does.

--- lib/queried_command-launcher.sh
+++ lib/queried_command-launcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly isSetWindowName="$1"; shift
+
 queriedCommand="$(tmux show-options -gv @queried_command)"
 [ -n "$queriedCommand" ] || exit 99
 
@@ -8,8 +10,17 @@ queriedCommand="$(tmux show-options -gv @queried_command)"
 defaultCommand="$(tmux show-options -Aqv default-command)"
 [ -z "$defaultCommand" ] || [ "$defaultCommand" != "$(tmux show-options -Aqv default-shell)" ] || defaultCommand=''
 
+typeset -a ensurePromptingArgs=()
+if [ "$isSetWindowName" ]; then
+    commandName="$(commandName --no-interpreter -- "$queriedCommand")"
+    tmux rename-window "$commandName"
+
+    printf -v quotedCommandName '%q' "(${commandName})"
+    ensurePromptingArgs+=(--prompt-command "tmux rename-window $quotedCommandName")
+fi
+
 PGID=$$ \
 TERM_COLORS=256 \
     exec \
-	ensurePrompting --prompt 'Press any key to dismiss...' -- \
+	ensurePrompting "${ensurePromptingArgs[@]}" --prompt 'Press any key to dismiss...' -- \
 	    runWithPrompt --command "${defaultCommand}${defaultCommand:+ }$queriedCommand"
